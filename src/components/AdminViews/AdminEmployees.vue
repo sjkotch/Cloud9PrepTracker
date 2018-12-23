e89af79f0d695c81aeda58afbae181178ba4034f
sjkotch <sjkotch@uvm.edu>
2018-12-23 16:18:46 -0500

    [FIX] Data table now visible

--- src/components/AdminViews/AdminEmployees.vue
+++ src/components/AdminViews/AdminEmployees.vue
@@ -4,7 +4,7 @@
     <v-content>
       <h1 class="page_title">Employees</h1>
       <v-btn @click="goBack" class="back_btn">Back</v-btn>
-      <v-flex>
+      <v-flex id="main_flex">
         <v-progress-circular
           id="loading_animation"
           v-if="!dataLoaded"
@@ -13,22 +13,42 @@
           :width=8
           indeterminate
         ></v-progress-circular>
-        <v-card v-if="dataLoaded">
-          <p>done</p>
-          <v-data-table id="chefs_data_table" :items="chefs" hide-headers hide-actions class="elevation-1">
-            <template slot="chefs" slot-scope="props">
-              <tr>
-                <td class="text-xs-left">{{ props.item.first_name + " " + props.item.last_name }}</td>
-                <td class="text-xs-left">{{ props.item.email }}</td>
-                <td class="text-xs-left">{{ props.item.phone }}</td>
-              </tr>
-            </template>
-          </v-data-table>
-        </v-card>
+        <v-flex v-if="dataLoaded">
+          <v-card id="main_card" elevation-1>
+            <p>add user</p>
+          </v-card>
+          <v-card v-if="dataLoaded" id="main_card" elevation-1>
+            <v-data-table id="chefs_data_table" :items="chefs" :headers="employeeHeaders" hide-actions class="elevation-1">
+              <template slot="items" slot-scope="props">
+                <tr @click="selectedEmployee(props.item)">
+                  <td class="text-xs-left cap">{{ props.item.name }}</td>
+                  <td class="text-xs-left cap">{{ props.item.nickname }}</td>
+                  <td class="text-xs-left">{{ props.item.email }}</td>
+                  <td class="text-xs-left cap">{{ props.item.user_type }}</td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-flex>
       </v-flex>
     </v-content>
   </v-app>
 </template>
+
+<style scoped>
+#main_card{
+  margin-left: 4em;
+  margin-right: 4em;
+}
+.cap{
+  text-transform: capitalize;
+}
+#main_flex{
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+}
+</style>
 
 <script>
 import { db } from '../../firebase.js'
@@ -36,9 +56,40 @@ import { db } from '../../firebase.js'
 export default {
   data () {
     return {
+      employeeHeaders: [
+        {
+          text: 'Name',
+          align: 'left',
+          value: 'name',
+          sortable: true
+        },
+        {
+          text: 'Nickname',
+          align: 'left',
+          value: 'nickname',
+          sortable: true
+        },
+        {
+          text: 'Email',
+          align: 'left',
+          value: 'email',
+          sortable: false
+        },
+        {
+          text: 'User Type',
+          align: 'left',
+          value: 'user_type',
+          sortable: true
+        }
+      ],
       chefs: [],
       dataLoaded: false
     }
+  },
+  computed: {
+    // getFullName (user) {
+    //   return (user.first_name + ' ' + user.last_name)
+    // }
   },
   created () {
     this.init()
@@ -53,15 +104,18 @@ export default {
           docs.forEach(doc => {
             let temp = doc.data()
             temp['id'] = doc.id
+            temp['name'] = temp.first_name + ' ' + temp.last_name
             this.chefs.push(temp)
             this.dataLoaded = true
-            console.log(this.chefs)
           })
         })
         .catch(err => console.error(err))
     },
     goBack () {
       this.$router.push('/')
+    },
+    selectedEmployee (user) {
+      // console.log(id)
     }
   }
 }
